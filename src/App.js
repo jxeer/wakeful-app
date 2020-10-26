af10554c7dc8eeeb99bc56ec378cd2e5e6b44401
Julian Xeer <julian.xeer@gmail.com>
2020-10-26 04:48:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+        <img src="/images/logo.png"></img>
+              <li>
+                <a href="#">COMMUNITY</a> 
+                <a href="#">CLASSES</a>
+                <a href="#">WHO WE ARE</a>
+                <a href="#">BLOG</a>
+                <a href="#">SIGN IN</a>
+                <a href="#">SEARCH</a>
+                <a href="#">FREE TRIAL</a>
+              </li>
+            </nav>
+          </header>
+          <br />
+          <br />
+          <br />
+          <main>
+            <div class="modalities">
+                Featured Modalities
+            </div>
+            <div class="teachers">
+              Featured Teachers
+            </div>
+            <div class="practicioners">
+              Featured Practicioners 
+            </div>
+          </main>
+          <br />
+          <br />
+          <footer>
+            Join newsletter <br />
+            links <br />
+            Social media <br />
+            Copyright <br />
+          </footer>
     </div>
   );
+
 }
+
 
 export default App;
